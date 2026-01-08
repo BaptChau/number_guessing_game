6b419d30f5f58fdbe8cf98d2129fea15ecd04fa9
Baptiste Chaudron <chaudron964@gmail.com>
2026-01-08 13:39:48 +0100

    feat: continue to play until the user is bored

--- main.py
+++ main.py
@@ -11,28 +11,29 @@ def get_number_of_tries(difficulty_input: int) -> int:
     return tries.value
 
 def main():
-    number_to_guess = select_number(limit=100)
-    print('Hello this is a guessing the number game \n the computer has chosen a number between 1 and 100')
-    difficulty_input = input("Choose a difficulty level:\n easy (1) -> 10 tries\n medium(2) -> 5 tries\n hard(3) -> 3 tries: \n")
-    difficulty_value =  get_number_of_tries(int(difficulty_input))
+    while True:
+        number_to_guess = select_number(limit=100)
+        print('Hello this is a guessing the number game \n the computer has chosen a number between 1 and 100')
+        difficulty_input = input("Choose a difficulty level:\n easy (1) -> 10 tries\n medium(2) -> 5 tries\n hard(3) -> 3 tries: \n\n Enter anything else to exit\n Your difficulty : ")
+        difficulty_value =  get_number_of_tries(int(difficulty_input))
 
-    if difficulty_value == 0:
-        print('You chose a false difficulty level')
-
-    tries = 0
-    while tries < difficulty_value:
-        value = int(input("Tell me your guess : \n\n"))
-        if value == number_to_guess:
-            print("Congratulations! You guessed the number!")
+        if difficulty_value == 0:
+            print('You chose a false difficulty level')
             return 0
-        tries += 1
-        if value > number_to_guess:
-            print('You guessed the number too high!')
-        elif value < number_to_guess:
-            print('You guessed the number too low!')
 
-    print('You did not guess the number, therefore you lose')
-    print('The number was ' + str(number_to_guess))
-    return 0
+        tries = 0
+        while tries < difficulty_value:
+            value = int(input("Tell me your guess : \n\n"))
+            if value == number_to_guess:
+                print("Congratulations! You guessed the number!")
+            tries += 1
+            if value > number_to_guess:
+                print('You guessed the number too high!')
+            elif value < number_to_guess:
+                print('You guessed the number too low!')
+
+        print('You did not guess the number, therefore you lose')
+        print('The number was ' + str(number_to_guess) + '\n\n')
+
 if __name__ == '__main__':
     main()
